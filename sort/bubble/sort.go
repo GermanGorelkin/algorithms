ba9938e277eb5bca65614c1891689c68720257d1
German Gorelkin <german.gorelkin@gmail.com>
2018-12-01 14:41:36 +0300

    add bubble-sort2

--- sort/bubble/sort.go
+++ sort/bubble/sort.go
@@ -11,3 +11,18 @@ func Sort(arr []int) {
 
 	}
 }
+
+func Sort2(arr []int) {
+	size := len(arr)
+	swapped := 1
+	for i := 0; i < size-1 && swapped == 1; i++ {
+		swapped = 0
+		for j := 0; j < size-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+				swapped = 1
+			}
+		}
+
+	}
+}
--- sort/bubble/sort_test.go
+++ sort/bubble/sort_test.go
@@ -35,3 +35,32 @@ func TestSort(t *testing.T) {
 		}
 	})
 }
+
+func TestSort2(t *testing.T) {
+	t.Run("simply", func(t *testing.T) {
+		arr := []int{5, 4, 3, 2, 1}
+		expected := []int{1, 2, 3, 4, 5}
+
+		Sort2(arr)
+
+		if !reflect.DeepEqual(arr, expected) {
+			t.Errorf("got %v\n want %v", arr, expected)
+		}
+	})
+
+	t.Run("random arr", func(t *testing.T) {
+		f := func(arr []int) bool {
+			expected := make([]int, len(arr))
+			copy(expected, arr)
+			sort.Ints(expected)
+
+			Sort2(arr)
+
+			return reflect.DeepEqual(arr, expected)
+		}
+
+		if err := quick.Check(f, nil); err != nil {
+			t.Error(err)
+		}
+	})
+}
