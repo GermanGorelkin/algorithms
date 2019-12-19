d332a444a207735577e913ebf2c5f40a2b9aaa40
German Gorelkin <german.gorelkin@gmail.com>
2019-12-19 12:04:54 +0300

    add AddHead

--- linkedlist/doubly/linkedlist.go
+++ linkedlist/doubly/linkedlist.go
@@ -1,5 +1,10 @@
 package doubly
 
+import (
+	"bytes"
+	"strconv"
+)
+
 type List struct {
 	head  *Node
 	tail  *Node
@@ -10,4 +15,39 @@ type Node struct {
 	value int
 	prev  *Node
 	next  *Node
+}
+
+func NewList() *List {
+	return &List{}
+}
+
+func (l *List) String() string {
+	b := bytes.Buffer{}
+	curr := l.head
+
+	b.WriteString("[")
+	for curr != nil {
+		b.WriteString(strconv.Itoa(curr.value))
+		curr = curr.next
+		if curr != nil {
+			b.WriteString(", ")
+		}
+	}
+	b.WriteString("]")
+	return b.String()
+}
+
+func (l *List) AddHead(val int) {
+	node := &Node{value: val}
+
+	if l.head == nil {
+		l.head = node
+		l.tail = node
+	} else {
+		node.next = l.head
+		l.head.prev = node
+		l.head = node
+	}
+
+	l.count++
 }
--- linkedlist/doubly/linkedlist_test.go
+++ linkedlist/doubly/linkedlist_test.go
@@ -1,1 +1,19 @@
 package doubly
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestList_AddHead(t *testing.T) {
+	l := NewList()
+
+	l.AddHead(1)
+	assert.Equal(t, "[1]", l.String())
+
+	l.AddHead(2)
+	assert.Equal(t, "[2, 1]", l.String())
+
+	l.AddHead(3)
+	assert.Equal(t, "[3, 2, 1]", l.String())
+}
