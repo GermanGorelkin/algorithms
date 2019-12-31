775845e77e02426271801b0eb7fe54729f709dce
German Gorelkin <german.gorelkin@gmail.com>
2019-12-31 10:54:14 +0300

    add String and ReverseString for doubly circular ll

---
+++ linkedlist/doubly-circular/linkedlist.go
@@ -0,0 +1,83 @@
+package doubly_circular
+
+import (
+	"bytes"
+	"errors"
+	"strconv"
+)
+
+var (
+	ErrEmptyList       = errors.New("list is empty")
+	ErrIndexOutOfRange = errors.New("index out of range")
+	ErrNodeNotFound    = errors.New("node not found")
+)
+
+type List struct {
+	head  *Node
+	tail  *Node
+	count int
+}
+
+type Node struct {
+	value int
+	prev  *Node
+	next  *Node
+}
+
+func (node *Node) Equals(other *Node) bool {
+	return node == other || node.value == other.value
+}
+
+func NewList() *List {
+	return &List{}
+}
+
+func (l *List) String() string {
+	b := bytes.Buffer{}
+	curr := l.head
+
+	b.WriteString("[")
+	for curr != nil {
+		b.WriteString(strconv.Itoa(curr.value))
+
+		curr = curr.next
+		if curr != l.head {
+			b.WriteString(", ")
+		} else {
+			break
+		}
+	}
+	b.WriteString("]")
+	return b.String()
+}
+
+func (l *List) ReverseString() string {
+	b := bytes.Buffer{}
+	curr := l.tail
+
+	b.WriteString("[")
+	for curr != nil {
+		b.WriteString(strconv.Itoa(curr.value))
+
+		curr = curr.prev
+		if curr != l.tail {
+			b.WriteString(", ")
+		} else {
+			break
+		}
+	}
+	b.WriteString("]")
+	return b.String()
+}
+
+func (l *List) Size() int {
+	return l.count
+}
+
+func (l *List) IsEmpty() bool {
+	return l.count == 0
+}
+
+func (l *List) AddHead(val int) {
+
+}
---
+++ linkedlist/doubly-circular/linkedlist_test.go
@@ -0,0 +1,80 @@
+package doubly_circular
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestList_String(t *testing.T) {
+	l := NewList()
+
+	t.Run("[]", func(t *testing.T) {
+		assert.Equal(t, "[]", l.String())
+	})
+	t.Run("[1]", func(t *testing.T) {
+		node := &Node{value: 1}
+		node.prev, node.next = node, node
+		l.tail, l.head = node, node
+
+		assert.Equal(t, "[1]", l.String())
+	})
+	t.Run("[1, 2]", func(t *testing.T) {
+		node1 := &Node{value: 1}
+		node2 := &Node{value: 2}
+
+		node1.prev, node1.next = node2, node2
+		node2.prev, node2.next = node1, node1
+		l.tail, l.head = node2, node1
+
+		assert.Equal(t, "[1, 2]", l.String())
+	})
+	t.Run("[1, 2, 3]", func(t *testing.T) {
+		node1 := &Node{value: 1}
+		node2 := &Node{value: 2}
+		node3 := &Node{value: 3}
+
+		node1.prev, node1.next = node3, node2
+		node2.prev, node2.next = node1, node3
+		node3.prev, node3.next = node2, node1
+		l.tail, l.head = node3, node1
+
+		assert.Equal(t, "[1, 2, 3]", l.String())
+	})
+}
+
+func TestList_ReverseString(t *testing.T) {
+	l := NewList()
+
+	t.Run("[]", func(t *testing.T) {
+		assert.Equal(t, "[]", l.ReverseString())
+	})
+	t.Run("[1]", func(t *testing.T) {
+		node := &Node{value: 1}
+		node.prev, node.next = node, node
+		l.tail, l.head = node, node
+
+		assert.Equal(t, "[1]", l.ReverseString())
+	})
+	t.Run("[1, 2]", func(t *testing.T) {
+		node1 := &Node{value: 1}
+		node2 := &Node{value: 2}
+
+		node1.prev, node1.next = node2, node2
+		node2.prev, node2.next = node1, node1
+		l.tail, l.head = node2, node1
+
+		assert.Equal(t, "[2, 1]", l.ReverseString())
+	})
+	t.Run("[1, 2, 3]", func(t *testing.T) {
+		node1 := &Node{value: 1}
+		node2 := &Node{value: 2}
+		node3 := &Node{value: 3}
+
+		node1.prev, node1.next = node3, node2
+		node2.prev, node2.next = node1, node3
+		node3.prev, node3.next = node2, node1
+		l.tail, l.head = node3, node1
+
+		assert.Equal(t, "[3, 2, 1]", l.ReverseString())
+	})
+}
