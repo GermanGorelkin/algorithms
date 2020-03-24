70b63ce09dbe904a70dd95c700a0984655c155c2
German Gorelkin <german.gorelkin@gmail.com>
2020-03-24 22:39:26 +0300

    add Number of Islands with DFS

--- graph/number-of-islands/number-of-islands.go
+++ graph/number-of-islands/number-of-islands.go
@@ -108,3 +108,36 @@ func (q *queue) deQueue() point {
 func (q *queue) isEmpty() bool {
 	return len(q.data) == 0
 }
+
+//---------V2
+
+func numIslands2(grid [][]byte) int {
+	if len(grid) == 0 {
+		return 0
+	}
+	var num int
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == 1 {
+				num++
+				dfs(grid, i, j)
+			}
+		}
+	}
+
+	return num
+}
+
+func dfs(grid [][]byte, i, j int) {
+	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || grid[i][j] == 0 {
+		return
+	}
+
+	grid[i][j] = 0
+
+	dfs(grid, i-1, j)
+	dfs(grid, i+1, j)
+	dfs(grid, i, j-1)
+	dfs(grid, i, j+1)
+}
--- graph/number-of-islands/number-of-islands_test.go
+++ graph/number-of-islands/number-of-islands_test.go
@@ -48,3 +48,47 @@ func Test_numIslands(t *testing.T) {
 		assert.Equal(t, want, numIslands(grid))
 	})
 }
+
+func Test_numIslands2(t *testing.T) {
+	t.Run("1", func(t *testing.T) {
+		grid := [][]byte{
+			{1, 1, 1, 1, 0},
+			{1, 1, 0, 1, 0},
+			{1, 1, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+		}
+		want := 1
+		assert.Equal(t, want, numIslands2(grid))
+	})
+	t.Run("2", func(t *testing.T) {
+		grid := [][]byte{
+			{1, 1, 0, 0, 0},
+			{1, 1, 0, 0, 0},
+			{0, 0, 1, 0, 0},
+			{0, 0, 0, 1, 1},
+		}
+		want := 3
+		assert.Equal(t, want, numIslands2(grid))
+	})
+	t.Run("3", func(t *testing.T) {
+		var grid [][]byte
+		want := 0
+		assert.Equal(t, want, numIslands2(grid))
+	})
+	t.Run("4", func(t *testing.T) {
+		grid := [][]byte{
+			{1, 0, 1, 0, 1},
+		}
+		want := 3
+		assert.Equal(t, want, numIslands2(grid))
+	})
+	t.Run("5", func(t *testing.T) {
+		grid := [][]byte{
+			{1, 1, 1},
+			{0, 1, 0},
+			{1, 1, 1},
+		}
+		want := 1
+		assert.Equal(t, want, numIslands2(grid))
+	})
+}
