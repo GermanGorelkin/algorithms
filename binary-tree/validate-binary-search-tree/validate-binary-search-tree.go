a71a8ceaa7a838a75acd73d9eeac8f06cc9f04bc
German Gorelkin <german.gorelkin@gmail.com>
2020-05-05 11:09:04 +0300

    add solve Validate BST with Iteration

--- binary-tree/validate-binary-search-tree/validate-binary-search-tree.go
+++ binary-tree/validate-binary-search-tree/validate-binary-search-tree.go
@@ -37,10 +37,15 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// Recursion
 func isValidBST(n *TreeNode) bool {
 	return checker(n, nil, nil)
 }
 
+/*
+Left ->  lower=lower;upper=val
+Right -> lower=val;upper=upper
+*/
 func checker(n *TreeNode, lower *int, upper *int) bool {
 	if n == nil {
 		return true
@@ -53,4 +58,59 @@ func checker(n *TreeNode, lower *int, upper *int) bool {
 		return false
 	}
 	return checker(n.Left, lower, &val) && checker(n.Right, &val, upper)
+}
+
+// Iteration
+func isValidBST_iter(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	st := &stack{}
+	lower := &stack{}
+	upper := &stack{}
+	st.push(root)
+	lower.push(nil)
+	upper.push(nil)
+
+	for !st.isEmpty() {
+		n := st.pop()
+		l := lower.pop()
+		u := upper.pop()
+
+		if l != nil && n.Val <= l.Val {
+			return false
+		}
+		if u != nil && n.Val >= u.Val {
+			return false
+		}
+
+		if n.Right != nil {
+			st.push(n.Right)
+			lower.push(n)
+			upper.push(u)
+		}
+		if n.Left != nil {
+			st.push(n.Left)
+			lower.push(l)
+			upper.push(n)
+		}
+	}
+
+	return true
+}
+
+type stack struct {
+	data []*TreeNode
+}
+
+func (st *stack) push(v *TreeNode) {
+	st.data = append(st.data, v)
+}
+func (st *stack) pop() *TreeNode {
+	v := st.data[len(st.data)-1]
+	st.data = st.data[:len(st.data)-1]
+	return v
+}
+func (st *stack) isEmpty() bool {
+	return len(st.data) == 0
 }
--- binary-tree/validate-binary-search-tree/validate-binary-search-tree_test.go
+++ binary-tree/validate-binary-search-tree/validate-binary-search-tree_test.go
@@ -61,3 +61,60 @@ func Test_isValidBST(t *testing.T) {
 		assert.False(t, isValidBST(n10))
 	})
 }
+
+func Test_isValidBST_iter(t *testing.T) {
+	t.Run("1", func(t *testing.T) {
+		/*
+		    2
+		   / \
+		  1   3
+		*/
+		n2 := &TreeNode{Val: 2}
+		n1 := &TreeNode{Val: 1}
+		n3 := &TreeNode{Val: 3}
+
+		n2.Left, n2.Right = n1, n3
+
+		assert.True(t, isValidBST_iter(n2))
+	})
+
+	t.Run("2", func(t *testing.T) {
+		/*
+		    5
+		   / \
+		  1   4
+		     / \
+		    3   6
+		*/
+		n5 := &TreeNode{Val: 5}
+		n1 := &TreeNode{Val: 1}
+		n4 := &TreeNode{Val: 4}
+		n3 := &TreeNode{Val: 3}
+		n6 := &TreeNode{Val: 6}
+
+		n5.Left, n5.Right = n1, n4
+		n4.Left, n4.Right = n3, n6
+
+		assert.False(t, isValidBST(n5))
+	})
+
+	t.Run("[10,5,15,null,null,6,20]", func(t *testing.T) {
+		/*
+		    10
+		   / \
+		  5  15
+		     / \
+		    6   20
+		*/
+		n10 := &TreeNode{Val: 10}
+		n5 := &TreeNode{Val: 5}
+		n15 := &TreeNode{Val: 15}
+		n6 := &TreeNode{Val: 6}
+		n20 := &TreeNode{Val: 20}
+
+		n10.Left, n10.Right = n5, n15
+		n15.Left, n15.Right = n6, n20
+
+		assert.False(t, isValidBST_iter(n10))
+	})
+}
