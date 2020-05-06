6376f86963f600c0563d5f7a272b9f5455cd910e
German Gorelkin <german.gorelkin@gmail.com>
2020-05-06 12:37:12 +0300

    fix Test_searchBST

--- binary-tree/search-in-a-binary-search-tree/search-in-a-binary-search-tree_test.go
+++ binary-tree/search-in-a-binary-search-tree/search-in-a-binary-search-tree_test.go
@@ -6,23 +6,23 @@ import (
 )
 
 func Test_searchBST(t *testing.T) {
-	t.Run("[3,9,20,null,null,15,7]", func(t *testing.T) {
+	t.Run("[3,9,20,null,null,15,21]", func(t *testing.T) {
 		/*
 		    3
 		   / \
 		  9  20
 		    /  \
-		   15   7
+		   15   21
 		*/
 
 		n3 := &TreeNode{Val: 3}
 		n9 := &TreeNode{Val: 9}
 		n20 := &TreeNode{Val: 20}
 		n15 := &TreeNode{Val: 15}
-		n7 := &TreeNode{Val: 7}
+		n21 := &TreeNode{Val: 21}
 
 		n3.Left, n3.Right = n9, n20
-		n20.Left, n20.Right = n15, n7
+		n20.Left, n20.Right = n15, n21
 
 		assert.Equal(t, n15, searchBST(n3, n15.Val))
 	})
