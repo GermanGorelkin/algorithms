817ab1a8ea76609cab936b5598f68560b670b1d0
German Gorelkin <german.gorelkin@gmail.com>
2020-05-06 12:07:51 +0300

    add Search in a Binary Search Tree

---
+++ binary-tree/search-in-a-binary-search-tree/search-in-a-binary-search-tree.go
@@ -0,0 +1,20 @@
+package search_in_a_binary_search_tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == val {
+		return root
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+	return searchBST(root.Right, val)
+}
---
+++ binary-tree/search-in-a-binary-search-tree/search-in-a-binary-search-tree_test.go
@@ -0,0 +1,29 @@
+package search_in_a_binary_search_tree
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_searchBST(t *testing.T) {
+	t.Run("[3,9,20,null,null,15,7]", func(t *testing.T) {
+		/*
+		    3
+		   / \
+		  9  20
+		    /  \
+		   15   7
+		*/
+
+		n3 := &TreeNode{Val: 3}
+		n9 := &TreeNode{Val: 9}
+		n20 := &TreeNode{Val: 20}
+		n15 := &TreeNode{Val: 15}
+		n7 := &TreeNode{Val: 7}
+
+		n3.Left, n3.Right = n9, n20
+		n20.Left, n20.Right = n15, n7
+
+		assert.Equal(t, n15, searchBST(n3, n15.Val))
+	})
+}
