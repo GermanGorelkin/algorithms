0d794048ce275e1b6756236ca94de53f6e797d81
German Gorelkin <german.gorelkin@gmail.com>
2020-08-06 14:52:25 +0300

    add Reverse Linked List v2

---
+++ linkedlist/reverse-linked-list/v2/reverse-linked-list.go
@@ -0,0 +1,24 @@
+package v2
+
+import (
+	ll "github.com/germangorelkin/algorithms/linkedlist"
+)
+
+/*
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+*/
+func reverseList(head *ll.ListNode) *ll.ListNode {
+	return reverse(head, nil)
+}
+
+func reverse(cur, prev *ll.ListNode) *ll.ListNode {
+	if cur == nil {
+		return prev
+	}
+
+	next := cur.Next
+	cur.Next = prev
+
+	return reverse(next, cur)
+}
