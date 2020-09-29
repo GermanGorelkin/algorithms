a0a7554da13f98b217aa0e4166affef76625ece1
German Gorelkin <german.gorelkin@gmail.com>
2020-09-29 20:46:01 +0300

    add code huffman decoding

---
+++ stepik1/code-huffman/decoding/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var lenMap, lenCode int
+	_, _ = fmt.Scanf("%d %d", &lenMap, &lenCode)
+	codeMap := make(map[string]string, lenMap)
+
+	for i := 0; i < lenMap; i++ {
+		var ch, code string
+		_, _ = fmt.Scanf("%s %s", &ch, &code)
+		codeMap[code] = string(ch[0])
+	}
+
+	var code string
+	_, _ = fmt.Scanf("%s", &code)
+
+	res := decode(code, codeMap)
+	fmt.Printf("%s", res)
+}
+
+func decode(code string, codeMap map[string]string) string {
+	var res string
+	i, j := 0, 1
+	for j <= len(code) {
+		if c, ok := codeMap[code[i:j]]; ok {
+			res += c
+			i, j = j, j+1
+		} else {
+			j++
+		}
+	}
+	return res
+}
