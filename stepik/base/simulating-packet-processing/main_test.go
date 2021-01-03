f4c14c5680734847bf6df10da445300595b8820e
German Gorelkin <german.gorelkin@gmail.com>
2021-01-03 23:00:57 +0300

    fix(simulating-packet-processing): fix test

--- stepik/base/simulating-packet-processing/main_test.go
+++ stepik/base/simulating-packet-processing/main_test.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_RingBuffer(t *testing.T) {
@@ -11,49 +12,49 @@ func Test_RingBuffer(t *testing.T) {
 	assert.True(t, rb.isEmpty())
 	assert.False(t, rb.isFull())
 
-	rb.enQueue(1)
+	rb.enQueue(packet{arrival: 1})
 	assert.False(t, rb.isEmpty())
 	assert.False(t, rb.isFull())
 
-	rb.enQueue(2)
+	rb.enQueue(packet{arrival: 2})
 	assert.False(t, rb.isEmpty())
 	assert.False(t, rb.isFull())
 
-	rb.enQueue(3)
+	rb.enQueue(packet{arrival: 3})
 	assert.False(t, rb.isEmpty())
 	assert.True(t, rb.isFull())
 
-	assert.Equal(t, 1, rb.deQueue())
+	assert.Equal(t, 1, rb.deQueue().arrival)
 	assert.False(t, rb.isEmpty())
 	assert.False(t, rb.isFull())
 
-	assert.Equal(t, 2, rb.deQueue())
+	assert.Equal(t, 2, rb.deQueue().arrival)
 	assert.False(t, rb.isEmpty())
 	assert.False(t, rb.isFull())
 
-	assert.Equal(t, 3, rb.deQueue())
+	assert.Equal(t, 3, rb.deQueue().arrival)
 	assert.True(t, rb.isEmpty())
 	assert.False(t, rb.isFull())
 
-	rb.enQueue(1)
-	rb.enQueue(2)
+	rb.enQueue(packet{arrival: 1})
+	rb.enQueue(packet{arrival: 2})
 	rb.deQueue()
-	rb.enQueue(3)
-	rb.enQueue(4)
+	rb.enQueue(packet{arrival: 3})
+	rb.enQueue(packet{arrival: 4})
 
-	assert.Equal(t, []int{4, 2, 3}, rb.data)
+	assert.Equal(t, []packet{{arrival: 4}, {arrival: 2}, {arrival: 3}}, rb.data)
 	assert.False(t, rb.isEmpty())
 	assert.True(t, rb.isFull())
 
-	assert.Equal(t, 2, rb.deQueue())
+	assert.Equal(t, 2, rb.deQueue().arrival)
 	assert.False(t, rb.isEmpty())
 	assert.False(t, rb.isFull())
 
-	assert.Equal(t, 3, rb.deQueue())
+	assert.Equal(t, 3, rb.deQueue().arrival)
 	assert.False(t, rb.isEmpty())
 	assert.False(t, rb.isFull())
 
-	assert.Equal(t, 4, rb.deQueue())
+	assert.Equal(t, 4, rb.deQueue().arrival)
 	assert.True(t, rb.isEmpty())
 	assert.False(t, rb.isFull())
 }
