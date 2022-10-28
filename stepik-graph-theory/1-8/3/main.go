ec61423ee2f94f1774d73c85705d5d09e41ff7c3
German Gorelkin <german.gorelkin@gmail.com>
2022-10-28 19:45:42 +0300

    solve stepik-graph 1.8.3

---
+++ stepik-graph-theory/1-8/3/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	list   [][]int
+	depths []int
+)
+
+func main() {
+	var N int
+	fmt.Scanf("%d ", &N)
+
+	list = make([][]int, N)
+	readTree(N)
+
+	depths = make([]int, N)
+
+	dfs(0)
+
+	for _, v := range depths {
+		fmt.Printf("%d ", v)
+	}
+
+	fmt.Println()
+}
+
+func dfs(v int) int {
+	depth := 1
+
+	for _, v := range list[v] {
+		depth = max(dfs(v)+1, depth)
+	}
+
+	depths[v] = depth
+
+	return depth
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+/*
+последовательность целых чисел p2..p3..pn,
+состоящая из n−1  числа, где pi равно номеру предка вершины с номером i.
+*/
+func readTree(n int) [][]int {
+	// i - child
+	// p - parent
+	for i := 1; i < n; i++ {
+		var p int
+		fmt.Scanf("%d", &p)
+
+		p--
+		list[p] = append(list[p], i)
+	}
+
+	return list
+}
+
+func PrintList(list [][]int) {
+	fmt.Println()
+	for _, l := range list {
+		fmt.Printf("%v\n", l)
+	}
+	fmt.Println()
+}
